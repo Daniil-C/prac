be22e5312e57fc2c8a15ba42a9f25233e21d6ac5
Daniil <chay-chitsd@mail.ru>
2020-04-21 12:38:08 +0300

    Add _()

--- 21042020/1.py
+++ 21042020/1.py
@@ -3,12 +3,12 @@ from tkinter import *
 from tkinter.messagebox import showinfo
  
 root = Tk()
-root.title("Login UI using Pack")
-root.geometry("400x320") # set starting size of window
-root.maxsize(400, 320) # width x height
+root.title(_("Login UI using Pack"))
+root.geometry("800x640") # set starting size of window
+root.maxsize(800, 640) # width x height
 root.config(bg="#6FAFE7") # set background color of root window
  
-login = Label(root, text="Login", bg="#2176C1", fg='white', relief=RAISED)
+login = Label(root, text=_("Login"), bg="#2176C1", fg='white', relief=RAISED)
 login.pack(ipady=5, fill='x')
 login.config(font=("Font", 30)) # change font and size of label
  
@@ -26,21 +26,21 @@ def checkInput():
     entered_pswrd = password_entry.get() # get password from Entry widget
  
     if (usernm == entered_usernm) and (pswrd == entered_pswrd):
-        showinfo("Hello", "Hello!")
+        showinfo(_("Hello"), _("Hello!"))
         root.destroy()  
     else:
-        showinfo("Hello", "Login failed: Invalid username or password.") 
+        showinfo(_("Hello"), _("Login failed: Invalid username or password.")) 
  
 def toggled():
     '''display a message to the terminal every time the check button 
     is clicked'''
-    showinfo("Hello", "The check button works.")
+    showinfo(_("Hello"), _("The check button works."))
  
 # Username Entry 
 username_frame = Frame(root, bg="#6FAFE7")
 username_frame.pack()
  
-Label(username_frame, text="Username", bg="#6FAFE7").pack(side='left', padx=5)
+Label(username_frame, text=_("Username"), bg="#6FAFE7").pack(side='left', padx=5)
 username_entry = Entry(username_frame, bd=3)
 username_entry.pack(side='right')
  
@@ -48,12 +48,12 @@ username_entry.pack(side='right')
 password_frame = Frame(root, bg="#6FAFE7")
 password_frame.pack()
  
-Label(password_frame, text="Password", bg="#6FAFE7").pack(side='left', padx=7)
+Label(password_frame, text=_("Password"), bg="#6FAFE7").pack(side='left', padx=7)
 password_entry = Entry(password_frame, bd=3)
 password_entry.pack(side='right')
  
 # Create Go! Button
-go_button = Button(root, text="GO!", command=checkInput, bg="#6FAFE7", width=15)
+go_button = Button(root, text=_("GO!"), command=checkInput, bg="#6FAFE7", width=15)
 go_button.pack(pady=5)
  
 # Remember me and forgot password
@@ -61,11 +61,11 @@ bottom_frame = Frame(root, bg="#6FAFE7")
 bottom_frame.pack()
  
 var = IntVar()
-remember_me = Checkbutton(bottom_frame, text="Remember me", bg="#6FAFE7", command=toggled, variable=var)
+remember_me = Checkbutton(bottom_frame, text=_("Remember me"), bg="#6FAFE7", command=toggled, variable=var)
 remember_me.pack(side='left', padx=19)
  
 # The forgot password Label is just a placeholder, has no function currently
-forgot_pswrd = Label(bottom_frame, text="Forgot password?", bg="#6FAFE7")
+forgot_pswrd = Label(bottom_frame, text=_("Forgot password?"), bg="#6FAFE7")
 forgot_pswrd.pack(side="right", padx=19)
  
 root.mainloop()
